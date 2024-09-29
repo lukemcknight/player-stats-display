b10b8f5e36bc6a68945f924dfef79c66963d9ae4
lmcknight25 <mckniglj@mail.uc.edu>
2024-09-29 12:10:50 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function getDate() {
   const today = new Date();
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, '0');
-  const date = String(today.getDate() - 1).padStart(2, '0');
+  const date = String(today.getDate()).padStart(2, '0');
   return `${year}${month}${date}`;
 }
 
